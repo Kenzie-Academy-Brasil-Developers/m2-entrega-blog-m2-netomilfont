4ffdd20f562bb5f77f79bac0b137d34a03d2ef9e
netomilfont <netomilfont@hotmail.com>
2022-08-25 16:38:36 -0300

    feat: requisicao login

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,6 +1,25 @@
 export class ApiRequest {
-    static async login() {
 
+    static BASEURL = "https://blog-m2.herokuapp.com"
+    static token = localStorage.getItem("@kenzieBlog:token") || ""
+    static headers = {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${this.token}`
+    }
+    static async login(body) {
+        const userLogin = await fetch(`${this.BASEURL}/users/login`, {
+            method: "POST",
+            headers: this.headers,
+            body: JSON.stringify(body)
+        })
+        .then(res => res.json())
+        .then(res => {
+            localStorage.setItem("@kenzieBlog:token", res.token)
+            localStorage.setItem("@kenzieBlog:User_id", res.user.id)
+        })
+        .catch(err => console.log(err))
+
+        return userLogin
     }
 
     static async cadastro() {
