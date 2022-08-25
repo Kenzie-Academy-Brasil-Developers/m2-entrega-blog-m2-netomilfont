fdffc5147468dc998148a933f3c100edb5b6235c
Pedro Mendes <106615792+pedroMendes182@users.noreply.github.com>
2022-08-25 14:22:57 -0300

    develop: add class de requisicoes

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,25 @@
 export class ApiRequest {
-    
+    static async login() {
+
+    }
+
+    static async cadastro() {
+
+    }
+
+    static async homePage() {
+
+    }
+
+    static async novoPost() {
+
+    }
+
+    static async editarPost() {
+
+    }
+
+    static async deletePost() {
+        
+    }
 }
