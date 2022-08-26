afac3542a5d9b648145988970c035d3bb723ca7f
netomilfont <netomilfont@hotmail.com>
2022-08-26 00:18:42 -0300

    feat: requisicoes para criar novo post

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -51,7 +51,15 @@ export class ApiRequest {
     }
 
     static async novoPost() {
+        const newPost = await fetch(`${ApiRequest.BASEURL}/posts`, {
+            method: "POST",
+            headers: this.headers,
+            body: JSON.stringify(body)
+        })
+        .then(res => res.json())
+        .catch(err => console.log(err))
 
+        return newPost
     }
 
     static async editarPost() {
